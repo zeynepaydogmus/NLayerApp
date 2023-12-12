66fad45d9fac48f23ba691ae88415980e364bf4e
Zeynep Aydoğmuş <aydgms.zeynep@gmail.com>
2023-12-13 00:25:59 +0300

    ErrorFilter

--- NLayer.Repository/Configuration/CategoryConfiguration.cs
+++ NLayer.Repository/Configuration/CategoryConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NLayer.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NLayer.Repository.Configuration
 {
@@ -15,8 +10,8 @@ namespace NLayer.Repository.Configuration
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.Property(x=>x.Name).IsRequired().HasMaxLength(50);
-            builder.ToTable("Categories");   
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.ToTable("Categories");
         }
     }
 }
---
+++ NLayer.Service/Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWork;
+
+namespace NLayer.Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
+        {
+            var products = await _productRepository.GetProductsWithCategory();
+            var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
+            return productsDto;
+        }
+    }
+}
